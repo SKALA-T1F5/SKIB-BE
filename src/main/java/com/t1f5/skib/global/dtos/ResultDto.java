ab5e692e6922d4cdfdb3f0e50f009547d2b98cfb
yoona <yoonalim2003@gmail.com>
2025-05-28 22:59:00 +0900

    feat: add resultDto

---
+++ src/main/java/com/t1f5/skib/global/dtos/ResultDto.java
@@ -0,0 +1,38 @@
+package com.t1f5.skib.global.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class ResultDto<T> {
+    private HttpStatus statusCode;
+    private String resultMsg;
+    private T resultData;
+
+    public ResultDto(final HttpStatus statusCode, final String resultMsg) {
+        this.statusCode = statusCode;
+        this.resultMsg = resultMsg;
+        this.resultData = null;
+    }
+
+    public static <T> ResultDto<T> res(final HttpStatus statusCode, final String resultMsg) {
+        return res(statusCode, resultMsg, null);
+    }
+
+    public static <T> ResultDto<T> res(final HttpStatus statusCode, final String resultMsg, final T t) {
+        return res(statusCode, resultMsg, t);
+    }
+
+    public static <T> ResultDto<T> res(final HttpStatus statusCode, final String resultMsg, final String resultDetail,
+            final T t) {
+        return ResultDto.<T>builder()
+                .statusCode(statusCode)
+                .resultMsg(resultMsg)
+                .resultData(t)
+                .build();
+    }
+}
