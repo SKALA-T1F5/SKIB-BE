6a8545e10d7d7b80858fbb95ab5e6732f7bee1cc
HyeonGyeong-Choi <hyengung99@gamil.com>
2025-05-29 19:24:44 +0900

    fix: Document>document & update README.md

--- src/main/java/com/t1f5/skib/Document/repository/DocumentRepository.java
+++ src/main/java/com/t1f5/skib/Document/repository/DocumentRepository.java
@@ -1,8 +1,8 @@
-package com.t1f5.skib.Document.repository;
+package com.t1f5.skib.document.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.t1f5.skib.Document.domain.Document;
+import com.t1f5.skib.document.domain.Document;
 
 public interface DocumentRepository extends JpaRepository<Document, Integer> {
 }
