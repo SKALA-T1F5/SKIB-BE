f7c15f100390b7b144ac88d202077d9295002980
HyeonGyeong-Choi <hyengung99@gamil.com>
2025-05-31 18:52:51 +0900

    feat: Add JWT authentication filter and custom user details service

---
+++ src/main/java/com/t1f5/skib/auth/model/CustomUserDetails.java
@@ -0,0 +1,51 @@
+package com.t1f5.skib.auth.model;
+
+import com.t1f5.skib.admin.model.Admin;
+import com.t1f5.skib.user.model.User;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+public class CustomUserDetails implements UserDetails{
+    private final String identifier; // email or id
+    private final String password;
+    private final String role;
+
+    public CustomUserDetails(String identifier, String password, String role) {
+        this.identifier = identifier;
+        this.password = password;
+        this.role = role;
+    }
+
+    public static CustomUserDetails fromUser(User user) {
+        return new CustomUserDetails(
+                user.getEmail(),
+                user.getPassword(),
+                user.getType().name()
+        );
+    }
+
+    public static CustomUserDetails fromAdmin(Admin admin) {
+        return new CustomUserDetails(
+                admin.getId().toString(),
+                admin.getPassword(),
+                "ADMIN"
+        );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role)); // 예: ROLE_ADMIN
+    }
+
+    @Override public String getUsername() { return identifier; }
+    @Override public boolean isAccountNonExpired() { return true; }
+    @Override public boolean isAccountNonLocked() { return true; }
+    @Override public boolean isCredentialsNonExpired() { return true; }
+    @Override public boolean isEnabled() { return true; }
+}
