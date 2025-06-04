10e81efe478d06b23661196eedabdb82cf3f2ff2
yoona <yoonalim2003@gmail.com>
2025-06-04 11:13:14 +0900

    feat: link userTest & User

---
+++ src/main/java/com/t1f5/skib/test/repository/UserTestRepository.java
@@ -0,0 +1,6 @@
+package com.t1f5.skib.test.repository;
+
+import com.t1f5.skib.test.domain.UserTest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserTestRepository extends JpaRepository<UserTest, Integer> {}
