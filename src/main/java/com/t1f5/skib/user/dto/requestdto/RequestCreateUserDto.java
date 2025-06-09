3d08ca7025f2516cc2b48ed54f18971099a98cab
HyeonGyeong-Choi <hyengung99@gamil.com>
2025-06-09 16:27:41 +0900

    fix: modify the method to allow creation of both trainers and trainees

--- src/main/java/com/t1f5/skib/user/dto/requestdto/RequestCreateUserDto.java
+++ src/main/java/com/t1f5/skib/user/dto/requestdto/RequestCreateUserDto.java
@@ -3,7 +3,12 @@ package com.t1f5.skib.user.dto.requestdto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
 import java.util.List;
+
+import com.t1f5.skib.global.enums.UserType;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,4 +25,7 @@ public class RequestCreateUserDto {
 
   @NotBlank(message = "비밀번호는 공백일 수 없습니다.")
   private String password;
+
+  @NotNull(message = "유저 타입은 반드시 필요합니다.")
+  private UserType type; // TRAINER 또는 TRAINEE
 }
