1b74b2239a6700f3e3eaf56e5246a7507d103d4c
yoona <yoonalim2003@gmail.com>
2025-06-25 16:40:45 +0900

    feat: Question 클래스에 tags 필드 및 GenerationType 열거형 추가

--- src/main/java/com/t1f5/skib/question/domain/Question.java
+++ src/main/java/com/t1f5/skib/question/domain/Question.java
@@ -1,6 +1,7 @@
 package com.t1f5.skib.question.domain;
 
 import com.t1f5.skib.global.enums.DifficultyLevel;
+import com.t1f5.skib.global.enums.GenerationType;
 import com.t1f5.skib.global.enums.QuestionType;
 import com.t1f5.skib.question.dto.GradingCriteriaDto;
 import java.util.List;
@@ -32,4 +33,5 @@ public class Question {
   private String documentName; // 문서 이름
   private List<String> keywords; // 키워드 목록 (예: 문서 제목, 주요 내용 등)
   private List<String> tags; // 태그 목록 (예: 문해력, 논리력 등)
+  private GenerationType generationType; // 문제 상태 (예: BASIC, EXTRA)
 }
